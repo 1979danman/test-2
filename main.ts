aa1693f7c5a99aeae16b5564d138dfd55ad4e5da
1979danman <141764433+1979danman@users.noreply.github.com>
2026-03-14 17:00:09 +0100

    Ton eingebaut

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 input.onPinPressed(TouchPin.P2, function () {
     endezeit = input.runningTime() + 5000
+    ton_gepielt = 0
 })
+let ton_gepielt = 0
 let endezeit = 0
 endezeit = 0
+ton_gepielt = 0
 basic.forever(function () {
     while (input.runningTime() < endezeit && pins.analogReadPin(AnalogReadWritePin.P1) > 500) {
         basic.showLeds(`
@@ -12,6 +15,12 @@ basic.forever(function () {
             . . . . #
             . . . . .
             `)
+        if (endezeit - input.runningTime() <= 2000 && ton_gepielt == 0) {
+            music.ringTone(262)
+            basic.pause(200)
+            music.stopAllSounds()
+            ton_gepielt = 1
+        }
     }
     basic.showLeds(`
         . . . . .
